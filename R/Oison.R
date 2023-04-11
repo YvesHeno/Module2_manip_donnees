9f55881825458678d13eb17ae72c5d6898b5ed57
YvesHeno <yves-marie.heno@ofb.gouv.fr>
2023-04-11 14:43:48 +0200

    filter

--- R/Oison.R
+++ R/Oison.R
@@ -24,6 +24,8 @@ load(file="Data/oison/table_bufo.Rdata")
 oison_taxon <- readr::read_csv(file="Data/oison/oison_taxon.csv")
 View(oison_taxon,"oison taxon")
 
+
+#select ----
 oison_taxon <- readr::read_csv(file="Data/oison/oison_taxon.csv") %>%
   dplyr::select(nom_vernaculaire,nom_scientifique,nom)
 View(oison_taxon,"oison taxon sélectionné")
@@ -68,8 +70,40 @@ oison_taxon <- readr::read_csv(file="Data/oison/oison_taxon.csv") %>%
   view()
 
 #pour voir autres cheat sheet dplyr
+
 #select(where(is.numeric))
+
+#filter ----
 oison_taxon <- readr::read_csv(file="Data/oison/oison_taxon.csv") %>%
   dplyr::filter(nom_scientifique=="Canis lupus") %>%
   dplyr::select(observation_id:nom_vernaculaire) %>%
   view()
+
+oison_taxon <- readr::read_csv(file="Data/oison/oison_taxon.csv") %>%
+  dplyr::filter(email=='camille.riviere@ofb.gouv.fr' | email=='didier.pujo@ofb.gouv.fr') %>%
+  view()
+
+oison_taxon <- readr::read_csv(file="Data/oison/oison_taxon.csv") %>%
+  dplyr::filter(presence=='Absent' & !is.na(nombre_individu)) %>%
+  select(observation_id,
+         date,
+         nom_scientifique,
+         nom_vernaculaire,
+         presence,
+         nombre_individu) %>%
+  filter(nombre_individu>0) %>% view()
+
+
+
+# utilisation de mutate ----
+oison_taxon <- readr::read_csv(file="Data/oison/oison_taxon.csv") %>%
+  select(observation_id,nom:email) %>%
+  mutate(nom_complet_observateur=paste(nom,prenom,sep=', '),addition=1+10,racine_carree=sqrt(observation_id))%>% view()
+
+oison_taxon <- readr::read_csv(file="Data/oison/oison_taxon.csv") %>%
+dplyr::select(nom_scientifique,nom_vernaculaire,nombre_individu) %>%
+  dplyr::filter(nom_scientifique=='Bufo bufo' | nom_scientifique=='Elona quimper') %>%
+  group_by(nom_scientifique) %>%
+  summarise(nb_total_oison=sum(nombre_individu,na.rm=T),
+            nb_max_oison=max(nombre_individu,na.rm=T)) %>% view()
+
